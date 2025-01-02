78e1bfec8f47f6bd0db69639363102a1bb69ec07
Valentin-Monod <vmonod5@gmail.com>
2025-01-02 14:30:25 +0100

    blinking solved + gesture gestion improved

--- src/Game_2048.scala
+++ src/Game_2048.scala
@@ -6,28 +6,48 @@ import java.awt.Color
 object Game_2048 extends App {
   val gameWindow = new FunGraphics(600, 600, "2048", true)
   val imageStop = new GraphicsBitmap("/res/stop.jpg")
+  var pressedUp, pressedDown, pressedLeft, pressedRight = false
   var offsetY = 0
   var offsetX = 0
   var stop = false
-  var factor = 30
+  var factor = 20
 
   gameWindow.setKeyManager(new KeyAdapter() {
+
     override def keyPressed(e: KeyEvent): Unit = {
       e.getKeyChar match {
-        case 'a' => offsetX -= 1
-        case 'd' => offsetX += 1
-        case 'w' => offsetY -= 1
-        case 's' => offsetY += 1
+        case 'a' => pressedLeft = true
+        case 'd' => pressedRight = true
+        case 'w' => pressedUp = true
+        case 's' => pressedDown = true
         case _ =>
       }
       e.getKeyCode match {
-        case KeyEvent.VK_LEFT => offsetX -= 1
-        case KeyEvent.VK_RIGHT => offsetX += 1
-        case KeyEvent.VK_UP => offsetY -= 1
-        case KeyEvent.VK_DOWN => offsetY += 1
+        case KeyEvent.VK_LEFT => pressedLeft = true
+        case KeyEvent.VK_RIGHT => pressedRight = true
+        case KeyEvent.VK_UP => pressedUp = true
+        case KeyEvent.VK_DOWN => pressedDown = true
         case _ =>
       }
     }
+
+    override def keyReleased(e: KeyEvent): Unit = {
+      e.getKeyChar match {
+        case 'a' => pressedLeft = false
+        case 'd' => pressedRight = false
+        case 'w' => pressedUp = false
+        case 's' => pressedDown = false
+        case _ =>
+      }
+      e.getKeyCode match {
+        case KeyEvent.VK_LEFT => pressedLeft = false
+        case KeyEvent.VK_RIGHT => pressedRight = false
+        case KeyEvent.VK_UP => pressedUp = false
+        case KeyEvent.VK_DOWN => pressedDown = false
+        case _ =>
+      }
+    }
+
   })
 
   gameWindow.addMouseListener(new MouseAdapter() {
@@ -44,11 +64,18 @@ object Game_2048 extends App {
   while (true) {
     if (stop) System.exit(0)
     else {
-      gameWindow.clear(Color.blue)
-      gameWindow.setColor(Color.red)
-      gameWindow.drawRect(250 + offsetX * factor, 250 + offsetY * factor, 75, 75)
-      gameWindow.drawTransformedPicture(100, 100, 0, 0.3, imageStop)
+      if (pressedDown) offsetY += 1
+      if (pressedUp) offsetY -= 1
+      if (pressedLeft) offsetX -= 1
+      if (pressedRight) offsetX += 1
+      gameWindow.frontBuffer.synchronized {
+        gameWindow.clear(Color.blue)
+        gameWindow.setColor(Color.red)
+        gameWindow.drawRect(250 + offsetX * factor, 250 + offsetY * factor, 75, 75)
+        gameWindow.drawTransformedPicture(100, 100, 0, 0.3, imageStop)
+      }
       gameWindow.syncGameLogic(60)
     }
   }
+
 }
