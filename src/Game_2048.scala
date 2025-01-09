ddf67fec3246063ce3e28a2cb7140226c571e3ea
Valentin-Monod <vmonod5@gmail.com>
2025-01-09 20:18:02 +0100

    update tabUpdate, gameLoop, getRandom and create a basic finish

--- src/Game_2048.scala
+++ src/Game_2048.scala
@@ -6,8 +6,7 @@ import scala.util.Random
 
 class Case() {
   var caseValue = 0
-  var step = 0
-  var nextValue = 0
+  var hasFusionned: Boolean = false
 }
 
 object Game_2048 extends App {
@@ -34,22 +33,23 @@ object Game_2048 extends App {
   // Keyboard - Directions
   var pressedUp, pressedDown, pressedLeft, pressedRight = false
   var direction = "none"
-  var isUpdating = false
 
-  // Game
-  var stop = false
+  // Game logic
+  var gameState = "playing" // "playing", "menu", "win", "lose"
+  var stop, isUpdating, win = false
   val stopSize = 50
+  var incr = 0
+  var tab: Array[Array[Case]] = Array.fill(gridSize, gridSize)(new Case)
+  tab = getRandomCases(tab)
+  tab = getRandomCases(tab)
+//  tab(0)(0).caseValue = 1024
+//  tab(0)(3).caseValue = 1024
+
+  // Images
   val tabMax = 2048
   val tabLength = (Math.log(tabMax * 2) / Math.log(2)).toInt
   val tabImages: Array[GraphicsBitmap] = new Array(tabLength)
   for (i <- 0 until tabLength) tabImages(i) = new GraphicsBitmap(s"/res/case${if (i == 0) 0 else Math.pow(2, i).toInt}.jpg")
-  var tab: Array[Array[Case]] = Array.fill(gridSize, gridSize)(new Case)
-  //tab = getRandomCases(tab)
-  //tab = getRandomCases(tab)
-  var x = 3
-  var y = 0
-  tab(y)(x).caseValue = 2
-  var incr = 0
   // -------------------------------------------------------------------------------------------------------------------
 
 
@@ -90,7 +90,7 @@ object Game_2048 extends App {
     }
   })
   gameWindow.addMouseListener(new MouseAdapter() {
-    override def mouseClicked(e: MouseEvent): Unit = {
+    override def mousePressed(e: MouseEvent): Unit = {
       val posx = e.getX
       val posy = e.getY
       if (posy < stopSize && posx > widthScreen - stopSize) stop = true
@@ -141,55 +141,135 @@ object Game_2048 extends App {
   def getRandomCases(tabValue: Array[Array[Case]]): Array[Array[Case]] = {
     var result: Array[Array[Case]] = tabValue.map(_.clone)
     var check = false
+
+    var check0 = false
+    for (y <- tabValue.indices; x <- tabValue(y).indices) {
+      if (tabValue(y)(x).caseValue == 0) check0 = true
+    }
+    if (!check0) check = true
+
     while (!check) {
-      var randomX = Random.nextInt(gridSize - 1)
-      var randomY = Random.nextInt(gridSize - 1)
-      if (result(randomX)(randomY).caseValue == 0) {
-        if (Random.nextInt(100) > 90) result(randomX)(randomY).caseValue = 4
-        else result(randomX)(randomY).caseValue = 2
+      var randomX = Random.nextInt(gridSize) // Génère un nombre entre 0 et 3
+      var randomY = Random.nextInt(gridSize) // Génère un nombre entre 0 et 3
+      if (result(randomY)(randomX).caseValue == 0) {
+        if (Random.nextInt(100) > 90) result(randomY)(randomX).caseValue = 4
+        else result(randomY)(randomX).caseValue = 2
         check = true
       }
     }
+
     result
   }
 
   def checkGame(tabValue: Array[Array[Case]]): Int = {
-    var result = 0 // Check si le jeu est perdu (2), gagné (1) ou si on continu (0)
+    var result = 0 // lose(2), win(1) playing(0)
+
     var check2048, check0 = false
     for (y <- tabValue.indices; x <- tabValue(y).indices) {
       if (tabValue(y)(x).caseValue == 2048) check2048 = true
       if (tabValue(y)(x).caseValue == 0) check0 = true
     }
+
     if (check2048) result = 1
     else {
-      if(!check0){
+      if (!check0) {
         var fusionPossible = false
         for (y <- tabValue.indices; x <- tabValue(y).indices) {
           try {
-            if (tabValue(y)(x).caseValue == tabValue(y+1)(x).caseValue ||
-              tabValue(y)(x).caseValue == tabValue(y)(x+1).caseValue ||
-              tabValue(y)(x).caseValue == tabValue(y-1)(x).caseValue ||
-              tabValue(y)(x).caseValue == tabValue(y)(x-1).caseValue){
+            if (
+              tabValue(y)(x).caseValue == tabValue(y + 1)(x).caseValue ||
+                tabValue(y)(x).caseValue == tabValue(y)(x + 1).caseValue ||
+                tabValue(y)(x).caseValue == tabValue(y - 1)(x).caseValue ||
+                tabValue(y)(x).caseValue == tabValue(y)(x - 1).caseValue) {
               fusionPossible = true
             }
           }
-          catch { case e : Exception => }
+          catch {
+            case e: Exception =>
+          }
         }
         if (!fusionPossible) result = 2
       }
     }
     result
   }
-  
+
   def updateTab(tabValue: Array[Array[Case]]): Array[Array[Case]] = {
-    var result: Array[Array[Case]] = Array.fill(gridSize, gridSize)(new Case)
-    if (incr < gridSize) {
-      // TODO :
+    var result: Array[Array[Case]] = tabValue.map(_.clone)
+    if (incr <= 7) {
       direction match {
-        case "down" => if (y < gridSize - 1) y += 1
-        case "up" => if (y > 0) y -= 1
-        case "left" => if (x > 0) x -= 1
-        case "right" => if (x < gridSize - 1) x += 1
+        case "down" =>
+          if (incr <= 2 || incr > 3) {
+            for (i <- 0 until gridSize; w <- (gridSize - 1) until 0 by -1) {
+              if (result(w)(i).caseValue == 0) {
+                result(w)(i).caseValue = result(w - 1)(i).caseValue
+                result(w - 1)(i).caseValue = 0
+              }
+            }
+          } else if (incr == 3) {
+            for (i <- 0 until gridSize; w <- (gridSize - 1) until 0 by -1) {
+              if (result(w)(i).caseValue == result(w - 1)(i).caseValue && !result(w)(i).hasFusionned && !result(w - 1)(i).hasFusionned) {
+                result(w)(i).caseValue *= 2
+                result(w)(i).hasFusionned = true
+                result(w - 1)(i).caseValue = 0
+              }
+            }
+          }
+
+        case "up" =>
+          if (incr <= 2 || incr > 3) {
+            for (i <- 0 until gridSize; w <- 0 until (gridSize - 1)) {
+              if (result(w)(i).caseValue == 0) {
+                result(w)(i).caseValue = result(w + 1)(i).caseValue
+                result(w + 1)(i).caseValue = 0
+              }
+            }
+          } else if (incr == 3) {
+            for (i <- 0 until gridSize; w <- 0 until (gridSize - 1)) {
+              if (result(w)(i).caseValue == result(w + 1)(i).caseValue && !result(w)(i).hasFusionned && !result(w + 1)(i).hasFusionned) {
+                result(w)(i).caseValue *= 2
+                result(w)(i).hasFusionned = true
+                result(w + 1)(i).caseValue = 0
+              }
+            }
+          }
+
+        case "left" =>
+          if (incr <= 2 || incr > 3) {
+            for (j <- 0 until gridSize; w <- 0 until (gridSize - 1)) {
+              if (result(j)(w).caseValue == 0) {
+                result(j)(w).caseValue = result(j)(w + 1).caseValue
+                result(j)(w + 1).caseValue = 0
+              }
+            }
+          } else if (incr == 3) {
+            for (j <- 0 until gridSize; w <- 0 until (gridSize - 1)) {
+              if (result(j)(w).caseValue == result(j)(w + 1).caseValue && !result(j)(w).hasFusionned && !result(j)(w + 1).hasFusionned) {
+                result(j)(w).caseValue *= 2
+                result(j)(w).hasFusionned = true
+                result(j)(w + 1).caseValue = 0
+              }
+            }
+          }
+
+        case "right" =>
+          if (incr <= 2 || incr > 3) {
+            for (j <- 0 until gridSize; w <- (gridSize - 1) until 0 by -1) {
+              if (result(j)(w).caseValue == 0) {
+                result(j)(w).caseValue = result(j)(w - 1).caseValue
+                result(j)(w - 1).caseValue = 0
+              }
+            }
+          } else if (incr == 3) {
+            for (j <- 0 until gridSize; w <- (gridSize - 1) until 0 by -1) {
+              if (result(j)(w).caseValue == result(j)(w - 1).caseValue && !result(j)(w).hasFusionned && !result(j)(w - 1).hasFusionned) {
+                result(j)(w).caseValue *= 2
+                result(j)(w).hasFusionned = true
+                result(j)(w - 1).caseValue = 0
+              }
+            }
+          }
+
         case _ =>
       }
       incr += 1
@@ -197,38 +277,18 @@ object Game_2048 extends App {
     else {
       incr = 0
       isUpdating = false
-      //tab = getRandomCases(tab)
+      direction = "none"
+      for (y <- tabValue.indices; x <- tabValue(y).indices) result(y)(x).hasFusionned = false
+      tab = getRandomCases(tab)
     }
-    result(y)(x).caseValue = 2
     return result
   }
 
-  def calculateNewFinalTab (tabValue: Array[Array[Case]]): Array[Array[Case]] = {
-    var result: Array[Array[Case]] = Array.fill(gridSize, gridSize)(new Case)
-    // TODO : calculer les distances et les valeurs finales
-    direction match {
-      case "down" =>
-        for (y <- tabValue.indices; x <- tabValue(y).indices){
-          for (w <- 0 to gridSize-y-1) {
-            if (tabValue(w)(x).caseValue != 0){
-              if (tabValue(w)(x).caseValue == tabValue(y)(x).caseValue){
-                tabValue(y)(x)
-              }
-            }
-            else tabValue(y)(x).step += 1
-          }
-        }
-      case "up" =>
-      case "left" =>
-      case "right" =>
-      case _ =>
-    }
-    result
-  }
-
-  def drawMenu(stateOfGame: Int): Unit = {
-    // TODO : faire une fonction draw menu qui dit si gagné ou perdu et si on veut rejouer
-    // aussi réinitialiser les variables si on rejoue
+  def drawMenu(): Unit = {
+    gameWindow.setColor(if (win) Color.green else Color.darkGray)
+    gameWindow.drawFillRect(150, 150, 300, 300)
+    val message = if (win) "You Win!" else "Game Over"
+    gameWindow.drawFancyString(170, 300, message, "Arial", 1, 50, Color.white)
   }
   // -------------------------------------------------------------------------------------------------------------------
 
@@ -238,22 +298,39 @@ object Game_2048 extends App {
     if (stop) System.exit(0)
     else {
       gameWindow.frontBuffer.synchronized {
-        if (checkGame(tab) == 0 || isUpdating) {
-          if (!isUpdating) {
-            if (pressedDown) direction = "down";
-            if (pressedUp) direction = "up"
-            if (pressedLeft) direction = "left"
-            if (pressedRight) direction = "right"
-            isUpdating = true
-            //calculateNewFinalTab(tab)
-          }
-          tab = updateTab(tab)
-          drawBackground()
-          drawTab(tab)
-        }
-        else {
-          drawBackground()
-          drawMenu(checkGame(tab))
+        gameState match {
+          case "menu" =>
+            drawBackground()
+            drawTab(tab)
+            drawMenu()
+
+          case "playing" =>
+            if (isUpdating) {
+              tab = updateTab(tab)
+              drawBackground()
+              drawTab(tab)
+            }
+            else if (checkGame(tab) == 0) {
+              if (pressedDown) direction = "down"
+              if (pressedUp) direction = "up"
+              if (pressedLeft) direction = "left"
+              if (pressedRight) direction = "right"
+              if (direction == "none") {
+                drawBackground()
+                drawTab(tab)
+              } else isUpdating = true
+            }
+            else {
+              gameState = if (checkGame(tab) == 2) "lose" else "win"
+            }
+
+          case "win" =>
+            win = true
+            gameState = "menu"
+
+          case "lose" =>
+            win = false
+            gameState = "menu"
         }
       }
       gameWindow.syncGameLogic(60)
