7b28989fa35a683c75e14fae1fcd92cf58a0630f
Valentin-Monod <vmonod5@gmail.com>
2025-01-13 13:17:25 +0100

    first complete version

--- src/Game_2048.scala
+++ src/Game_2048.scala
@@ -1,4 +1,4 @@
-import java.awt.Color
+import java.awt.{Color}
 import scala.util.Random
 import hevs.graphics.FunGraphics
 import hevs.graphics.utils.GraphicsBitmap
@@ -18,35 +18,40 @@ object Game_2048 extends App {
   val padding = 20
   val cellSize = (widthScreen - (2 * margin) - (gridSize * padding)) / gridSize
   val caseFactor = (cellSize.toDouble / 120.0)
+  val menuBtnSize = 50
 
   // Colors
-  val backColor = new Color(187, 173, 160)
+  val gameBackColor = new Color(187, 173, 160)
+  val menuBackColor = new Color(187, 173, 160, 208)
   val caseColor = new Color(202, 192, 180)
   val titleColor = new Color(117, 110, 101)
-  var stopColor = Color.darkGray
+  val neutralColor = new Color(117, 110, 101)
+  val winColor = new Color(67, 125, 81)
+  val newGameBtnColorNormal = new Color(242, 177, 121)
+  val newGameBtnColorHover = new Color(199, 146, 99)
+  val leaveBtnColorHover = new Color(217, 108, 108)
 
-  // Keyboard - Directions
+  var leaveBtnColor = caseColor
+  var newGameBtnColor = newGameBtnColorNormal
+  var btnMenuColor = titleColor
+
+  // Keyboard
   var pressedUp, pressedDown, pressedLeft, pressedRight = false
-  var direction = "none"
-
-  // Game logic
-  var gameState = "playing" // "playing", "menu", "win", "lose"
-  var stop, isUpdating, win = false
-  val stopSize = 50
-  var incr = 0
-  var tab: Array[Array[Case]] = Array.fill(gridSize, gridSize)(new Case)
-  tab = getRandomCases(tab)
-  tab = getRandomCases(tab)
-
-  // To test the win easly
-  //tab(0)(0).caseValue = 1024
-  //tab(0)(3).caseValue = 1024
 
   // Images
   val tabMax = 2048
   val tabLength = (Math.log(tabMax * 2) / Math.log(2)).toInt
   val tabImages: Array[GraphicsBitmap] = new Array(tabLength)
   for (i <- 0 until tabLength) tabImages(i) = new GraphicsBitmap(s"/res/case${if (i == 0) 0 else Math.pow(2, i).toInt}.jpg")
+
+  // Game logic
+  var gameState = "start" // "playing", "menu", "win", "lose", "start"
+  var direction = "none"
+  var isUpdating, win, start = false
+  var incr = 0
+  var tab: Array[Array[Case]] = Array.fill(gridSize, gridSize)(new Case)
+  tab = getRandomCases(tab)
+  tab = getRandomCases(tab)
   // -------------------------------------------------------------------------------------------------------------------
 
 
@@ -90,16 +95,43 @@ object Game_2048 extends App {
     override def mousePressed(e: MouseEvent): Unit = {
       val posx = e.getX
       val posy = e.getY
-      if (posy < stopSize && posx > widthScreen - stopSize) stop = true
+      gameState match {
+        case "start" =>
+          if (posy < 392 && posy > 275 && posx < 450 && posx > 150) {
+            gameState = "playing"
+            start = true
+          }
+          if (posy < 570 && posy > 450 && posx < 450 && posx > 150) System.exit(0)
+        case "menu" =>
+          if (posy < menuBtnSize && posx > widthScreen - menuBtnSize) gameState = "playing"
+          if (posy < 392 && posy > 275 && posx < 450 && posx > 150) restart()
+          if (posy < 570 && posy > 450 && posx < 450 && posx > 150) System.exit(0)
+        case "playing" =>
+          if (posy < menuBtnSize && posx > widthScreen - menuBtnSize) gameState = "menu"
+      }
     }
   })
   gameWindow.addMouseMotionListener(new MouseMotionAdapter() {
     override def mouseMoved(e: MouseEvent): Unit = {
       val posx = e.getX
       val posy = e.getY
-      if (posy < stopSize && posx > widthScreen - stopSize) stopColor = Color.red
-      else stopColor = Color.darkGray
-      //println(s"Mouse : (X:$posx, Y:$posy)")
+      gameState match {
+        case "start" =>
+          if (posy < 392 && posy > 275 && posx < 450 && posx > 150) newGameBtnColor = newGameBtnColorHover
+          else newGameBtnColor = newGameBtnColorNormal
+          if (posy < 570 && posy > 450 && posx < 450 && posx > 150) leaveBtnColor = leaveBtnColorHover
+          else leaveBtnColor = caseColor
+        case "menu" =>
+          if (posy < menuBtnSize && posx > widthScreen - menuBtnSize) btnMenuColor = caseColor
+          else btnMenuColor = titleColor
+          if (posy < 392 && posy > 275 && posx < 450 && posx > 150) newGameBtnColor = newGameBtnColorHover
+          else newGameBtnColor = newGameBtnColorNormal
+          if (posy < 570 && posy > 450 && posx < 450 && posx > 150) leaveBtnColor = leaveBtnColorHover
+          else leaveBtnColor = caseColor
+        case "playing" =>
+          if (posy < menuBtnSize && posx > widthScreen - menuBtnSize) btnMenuColor = caseColor
+          else btnMenuColor = titleColor
+      }
     }
   })
   // -------------------------------------------------------------------------------------------------------------------
@@ -108,15 +140,16 @@ object Game_2048 extends App {
   // ----------------------------------------------------------------------------------------------------------Fonctions
   def drawBackground(): Unit = {
     // Background
-    gameWindow.clear(backColor)
+    gameWindow.clear(gameBackColor)
     // Title
     gameWindow.drawFancyString(215, 85, "2048", "Arial Rounded MT Bold", 1, 70, titleColor)
-    // Stop BTN
-    gameWindow.setColor(stopColor)
-    gameWindow.drawFillRect(widthScreen - stopSize, 0, stopSize, stopSize)
+    // Menu BTN
+    gameWindow.setColor(btnMenuColor)
+    gameWindow.drawFillRect(widthScreen - menuBtnSize, 0, menuBtnSize, menuBtnSize)
     gameWindow.setColor(Color.white)
-    gameWindow.drawLine(widthScreen - stopSize, 0, widthScreen, stopSize)
-    gameWindow.drawLine(widthScreen - stopSize, stopSize, widthScreen, 0)
+    gameWindow.drawLine(widthScreen - menuBtnSize + menuBtnSize / 6, (menuBtnSize / 6) * 2, widthScreen - menuBtnSize / 6, (menuBtnSize / 6) * 2)
+    gameWindow.drawLine(widthScreen - menuBtnSize + menuBtnSize / 6, (menuBtnSize / 6) * 3, widthScreen - menuBtnSize / 6, (menuBtnSize / 6) * 3)
+    gameWindow.drawLine(widthScreen - menuBtnSize + menuBtnSize / 6, (menuBtnSize / 6) * 4, widthScreen - menuBtnSize / 6, (menuBtnSize / 6) * 4)
     // Game Cells
     gameWindow.setColor(caseColor)
     for (x <- 0 until gridSize; y <- 0 until gridSize) {
@@ -193,17 +226,17 @@ object Game_2048 extends App {
 
   def updateTab(tabValue: Array[Array[Case]]): Array[Array[Case]] = {
     var result: Array[Array[Case]] = tabValue.map(_.clone)
-    if (incr <= (gridSize*2)-1) {
+    if (incr <= (gridSize * 2) - 1) {
       direction match {
         case "down" =>
-          if (incr < gridSize-1 || incr > gridSize-1) {
+          if (incr < gridSize - 1 || incr > gridSize - 1) {
             for (i <- 0 until gridSize; w <- (gridSize - 1) until 0 by -1) {
               if (result(w)(i).caseValue == 0) {
                 result(w)(i).caseValue = result(w - 1)(i).caseValue
                 result(w - 1)(i).caseValue = 0
               }
             }
-          } else if (incr == gridSize-1) {
+          } else if (incr == gridSize - 1) {
             for (i <- 0 until gridSize; w <- (gridSize - 1) until 0 by -1) {
               if (result(w)(i).caseValue == result(w - 1)(i).caseValue && !result(w)(i).hasFusionned && !result(w - 1)(i).hasFusionned) {
                 result(w)(i).caseValue *= 2
@@ -214,14 +247,14 @@ object Game_2048 extends App {
           }
 
         case "up" =>
-          if (incr < gridSize-1 || incr > gridSize-1) {
+          if (incr < gridSize - 1 || incr > gridSize - 1) {
             for (i <- 0 until gridSize; w <- 0 until (gridSize - 1)) {
               if (result(w)(i).caseValue == 0) {
                 result(w)(i).caseValue = result(w + 1)(i).caseValue
                 result(w + 1)(i).caseValue = 0
               }
             }
-          } else if (incr == gridSize-1) {
+          } else if (incr == gridSize - 1) {
             for (i <- 0 until gridSize; w <- 0 until (gridSize - 1)) {
               if (result(w)(i).caseValue == result(w + 1)(i).caseValue && !result(w)(i).hasFusionned && !result(w + 1)(i).hasFusionned) {
                 result(w)(i).caseValue *= 2
@@ -232,14 +265,14 @@ object Game_2048 extends App {
           }
 
         case "left" =>
-          if (incr < gridSize-1 || incr > gridSize-1) {
+          if (incr < gridSize - 1 || incr > gridSize - 1) {
             for (j <- 0 until gridSize; w <- 0 until (gridSize - 1)) {
               if (result(j)(w).caseValue == 0) {
                 result(j)(w).caseValue = result(j)(w + 1).caseValue
                 result(j)(w + 1).caseValue = 0
               }
             }
-          } else if (incr == gridSize-1) {
+          } else if (incr == gridSize - 1) {
             for (j <- 0 until gridSize; w <- 0 until (gridSize - 1)) {
               if (result(j)(w).caseValue == result(j)(w + 1).caseValue && !result(j)(w).hasFusionned && !result(j)(w + 1).hasFusionned) {
                 result(j)(w).caseValue *= 2
@@ -250,14 +283,14 @@ object Game_2048 extends App {
           }
 
         case "right" =>
-          if (incr < gridSize-1 || incr > gridSize-1) {
+          if (incr < gridSize - 1 || incr > gridSize - 1) {
             for (j <- 0 until gridSize; w <- (gridSize - 1) until 0 by -1) {
               if (result(j)(w).caseValue == 0) {
                 result(j)(w).caseValue = result(j)(w - 1).caseValue
                 result(j)(w - 1).caseValue = 0
               }
             }
-          } else if (incr == gridSize-1) {
+          } else if (incr == gridSize - 1) {
             for (j <- 0 until gridSize; w <- (gridSize - 1) until 0 by -1) {
               if (result(j)(w).caseValue == result(j)(w - 1).caseValue && !result(j)(w).hasFusionned && !result(j)(w - 1).hasFusionned) {
                 result(j)(w).caseValue *= 2
@@ -282,56 +315,97 @@ object Game_2048 extends App {
   }
 
   def drawMenu(): Unit = {
+    // Background Menu
+    gameWindow.setColor(menuBackColor)
+    gameWindow.drawFillRect(margin * 4, menuScreen + margin * 4, widthScreen - (8 * margin), widthScreen - (8 * margin))
+    // New Game Button
+    gameWindow.setColor(newGameBtnColor)
+    gameWindow.drawFillRect(widthScreen / 4, menuScreen + heightScreen / 4, widthScreen / 2, heightScreen / 6)
+    gameWindow.drawFancyString(170, 350, "New Game", "Arial Rounded MT Bold", 1, 50, Color.white)
+    // Leave Button
+    gameWindow.setColor(leaveBtnColor)
+    gameWindow.drawFillRect(widthScreen / 4, menuScreen + heightScreen / 2, widthScreen / 2, heightScreen / 6)
+    gameWindow.drawFancyString(225, 525, "Leave", "Arial Rounded MT Bold", 1, 50, Color.black)
     gameWindow.setColor(if (win) Color.green else Color.darkGray)
-    gameWindow.drawFillRect(150, 150, 300, 300)
-    val message = if (win) "You Win!" else "Game Over"
-    gameWindow.drawFancyString(170, 300, message, "Arial", 1, 50, Color.white)
+    // Text win, lose, playing
+    if (start) gameWindow.drawFancyString(110, 245, "Game running...", "Arial", 1, 50, neutralColor)
+    else if (win) gameWindow.drawFancyString(190, 245, "You win !", "Arial", 1, 50, winColor)
+    else gameWindow.drawFancyString(180, 245, "You lose !", "Arial", 1, 50, leaveBtnColorHover)
+  }
+
+  def drawStart(): Unit = {
+    // Background
+    gameWindow.clear(gameBackColor)
+    // Title
+    gameWindow.drawFancyString(215, 85, "2048", "Arial Rounded MT Bold", 1, 70, titleColor)
+    // New Game Button
+    gameWindow.setColor(newGameBtnColor)
+    gameWindow.drawFillRect(widthScreen / 4, menuScreen + heightScreen / 4, widthScreen / 2, heightScreen / 6)
+    gameWindow.drawFancyString(170, 350, "New Game", "Arial Rounded MT Bold", 1, 50, Color.white)
+    // Leave Button
+    gameWindow.setColor(leaveBtnColor)
+    gameWindow.drawFillRect(widthScreen / 4, menuScreen + heightScreen / 2, widthScreen / 2, heightScreen / 6)
+    gameWindow.drawFancyString(225, 525, "Leave", "Arial Rounded MT Bold", 1, 50, Color.black)
+  }
+
+  def restart(): Unit = {
+    gameState = "playing"
+    direction = "none"
+    isUpdating = false
+    start = true
+    win = false
+    incr = 0
+    tab = Array.fill(gridSize, gridSize)(new Case)
+    tab = getRandomCases(tab)
+    tab = getRandomCases(tab)
   }
   // -------------------------------------------------------------------------------------------------------------------
 
 
   // -----------------------------------------------------------------------------------------------------------GameLoop
   while (true) {
-    if (stop) System.exit(0)
-    else {
-      gameWindow.frontBuffer.synchronized {
-        gameState match {
-          case "menu" =>
+    gameWindow.frontBuffer.synchronized {
+      gameState match {
+        case "start" =>
+          drawStart()
+
+        case "menu" =>
+          drawBackground()
+          drawTab(tab)
+          drawMenu()
+
+        case "playing" =>
+          if (isUpdating) {
+            tab = updateTab(tab)
             drawBackground()
             drawTab(tab)
-            drawMenu()
-
-          case "playing" =>
-            if (isUpdating) {
-              tab = updateTab(tab)
+          }
+          else if (checkGame(tab) == 0) {
+            if (pressedDown) direction = "down"
+            if (pressedUp) direction = "up"
+            if (pressedLeft) direction = "left"
+            if (pressedRight) direction = "right"
+            if (direction == "none") {
               drawBackground()
               drawTab(tab)
-            }
-            else if (checkGame(tab) == 0) {
-              if (pressedDown) direction = "down"
-              if (pressedUp) direction = "up"
-              if (pressedLeft) direction = "left"
-              if (pressedRight) direction = "right"
-              if (direction == "none") {
-                drawBackground()
-                drawTab(tab)
-              } else isUpdating = true
-            }
-            else {
-              gameState = if (checkGame(tab) == 2) "lose" else "win"
-            }
+            } else isUpdating = true
+          }
+          else {
+            gameState = if (checkGame(tab) == 2) "lose" else "win"
+          }
 
-          case "win" =>
-            win = true
-            gameState = "menu"
+        case "win" =>
+          win = true
+          start = false
+          gameState = "menu"
 
-          case "lose" =>
-            win = false
-            gameState = "menu"
-        }
+        case "lose" =>
+          win = false
+          start = false
+          gameState = "menu"
       }
-      gameWindow.syncGameLogic(60)
     }
+    gameWindow.syncGameLogic(60)
   }
   // -------------------------------------------------------------------------------------------------------------------
 }
